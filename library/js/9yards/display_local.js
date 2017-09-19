a881f2a0a43a234a81fa91bf903e26211853446c
buckaroosupport <buckaroosupport@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2017-09-19 12:14:54 +0000

    = 2.5.0 = Admin modernised/improved - Option to use single certificate across all payment methods added - Certificates now added via 'upload Certificate' button - Certificates now stored in database, rather than as network file - Master settings page added, allows centralised setting of - Language - Transaction Description - Use notification - Notification Delay - Merchant Key - Secret Key - Thumbrint - Certificate - Currency - Transaction Mode - Debug Mode added (records requests to & responses from buckaroo) - Code modernised/imporved - Docblocks added - Old commented code removed - Calls to WooCommerce & Buckaroo centralised in wrapper functions - Include/require function centralised -Push Notifications Fixed -Missing translations added
    git-svn-id: https://plugins.svn.wordpress.org/wc-buckaroo-bpe-gateway/trunk@1732197 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ library/js/9yards/display_local.js
@@ -0,0 +1,61 @@
+jQuery(document).ready(function() {
+    //Get Location
+    var label = jQuery('label').first().attr("for");
+    var label = label.substring(0, label.lastIndexOf('_'));
+    var locationName = label.substring(label.lastIndexOf('_'), label.length);
+    
+    var usemaster = jQuery("#woocommerce_buckaroo"+locationName+"_usemaster");
+    var checked = (usemaster.attr('checked') ? 1 : 0);
+    if (usemaster.length != 0){
+        
+        function togglelocal(checked){
+            // console.log('usemaster:'+checked);
+
+            var merchantKeyTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_merchantkey").closest('tr');
+            var secretKeyTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_secretkey").closest('tr');
+            var thumbprintTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_thumbprint").closest('tr');
+            var uploadButtonTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_upload").closest('tr');
+            // var modeTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_mode").closest('tr');
+            var currencyTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_currency").closest('tr');
+            var selectCertificateTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_selectcertificate").closest('tr');
+            var cultureTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_culture").closest('tr');
+            var transactionDescriptionTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_transactiondescription").closest('tr');
+            var useNotificationTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_usenotification").closest('tr');
+            var notificationDelayTR = document.querySelector("#woocommerce_buckaroo"+locationName+"_notificationdelay").closest('tr');
+            if (checked == 1) {
+                merchantKeyTR.style.display = "none";
+                secretKeyTR.style.display = "none";
+                thumbprintTR.style.display = "none";
+                uploadButtonTR.style.display = "none";
+                // modeTR.style.display = "none";
+                currencyTR.style.display = "none";
+                selectCertificateTR.style.display = "none";
+                cultureTR.style.display = "none";
+                transactionDescriptionTR.style.display = "none";
+                useNotificationTR.style.display = "none";
+                notificationDelayTR.style.display = "none";
+            } else {
+                merchantKeyTR.style.display = "table-row";
+                secretKeyTR.style.display = "table-row";
+                thumbprintTR.style.display = "table-row";
+                uploadButtonTR.style.display = "table-row";
+                // modeTR.style.display = "table-row";
+                currencyTR.style.display = "table-row";
+                selectCertificateTR.style.display = "table-row";
+                cultureTR.style.display = "table-row";
+                transactionDescriptionTR.style.display = "table-row";
+                useNotificationTR.style.display = "table-row";
+                notificationDelayTR.style.display = "table-row";
+            }
+            return checked;
+        }
+
+        //Initialise correct display
+        togglelocal(checked);
+        //Watch for clicks that tick/untick
+        jQuery(usemaster).click(function() {
+            checked = (checked == 1 ? 0 : 1);
+            togglelocal(checked);
+        });
+    }
+});
