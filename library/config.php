2e92b6988cbe846393bf1767917f3a7dd4b553c1
buckaroosupport <buckaroosupport@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2019-06-19 10:39:55 +0000

    New release of the Buckaroo plugin, see readme.txt for release details
    git-svn-id: https://plugins.svn.wordpress.org/wc-buckaroo-bpe-gateway/trunk@2108796 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- library/config.php
+++ library/config.php
@@ -7,7 +7,7 @@ require_once dirname(__FILE__).'/api/config/configcore.php';
 class BuckarooConfig extends BuckarooConfigCore {
     const NAME = 'buckaroo3';
     const PLUGIN_NAME = 'Buckaroo BPE 3.0 official plugin';
-    const VERSION = '2.12.0';
+    const VERSION = '2.12.1';
 
     const SHIPPING_SKU = "WC8888";
    
