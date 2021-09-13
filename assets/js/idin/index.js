cf7eaeaa5182259196337ad3d2604be210031554
buckaroosupport <buckaroosupport@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2021-09-13 07:37:13 +0000

    BP-1050 WooCommerce - iDIN verification does not work
    git-svn-id: https://plugins.svn.wordpress.org/wc-buckaroo-bpe-gateway/branches/develop@2597812 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- assets/js/idin/index.js
+++ assets/js/idin/index.js
@@ -1,68 +1,73 @@
-function buckarooDisableBlock(blockSelector) {
-  $(blockSelector).block({
-    message: null,
-    overlayCSS: {
-      background: '#fff',
-      opacity: 0.6
-    }
-  });
-}
+(function( $ ) {
 
-function buckarooIdentify(issuer) {
-  buckarooDisableBlock('.checkout.woocommerce-checkout');
+  function buckarooDisableBlock(blockSelector) {
+    $(blockSelector).block({
+      message: null,
+      overlayCSS: {
+        background: '#fff',
+        opacity: 0.6
+      }
+    });
+  }
 
-  setTimeout(() => {
-    $.ajax({
-      url: "/",
-      data: {
-        "wc-api": "WC_Gateway_Buckaroo_idin-identify",
-        "issuer": issuer
-      },
-      async: false,
-      dataType: "json"
-    })
-    .done((response) => {
-      if (response) {
-        if (response.result == "success") {
-          window.location.replace(response.redirect);
-          $('.woocommerce-checkout').unblock();
-          return true;
-        } else {
-          if (response.message) {
-            buckarooDisplayErrorMessage(response.message);
+  function buckarooIdentify(issuer) {
+    buckarooDisableBlock('.checkout.woocommerce-checkout');
+
+    setTimeout(() => {
+      $.ajax({
+        url: "/",
+        data: {
+          "wc-api": "WC_Gateway_Buckaroo_idin-identify",
+          "issuer": issuer
+        },
+        async: false,
+        dataType: "json"
+      })
+      .done((response) => {
+        if (response) {
+          if (response.result == "success") {
+            window.location.replace(response.redirect);
             $('.woocommerce-checkout').unblock();
-            return false;
+            return true;
+          } else {
+            if (response.message) {
+              buckarooDisplayErrorMessage(response.message);
+              $('.woocommerce-checkout').unblock();
+              return false;
+            }
           }
         }
-      }
-      buckarooDisplayErrorMessage("Something went wrong while processing your identification.");
-      $('.woocommerce-checkout').unblock();
-    })
-    .fail(() => {
-      buckarooDisplayErrorMessage("Something went wrong while processing your identification.");
-      $('.woocommerce-checkout').unblock();
-    });
-  },1000);
-}
-
-function buckarooDisplayErrorMessage(message) {
-  const content = `      
-      <div class="woocommerce-error" role="alert">
-        ${message}
-      </div>
-    `;
-  $('.woocommerce-notices-wrapper').first().prepend(content);
-  var wooError = $('.woocommerce-notices-wrapper .woocommerce-error').first();
-  setTimeout(function(){
-    wooError.fadeOut(1000);
-  }, 10000);
-  $('html, body').scrollTop(0);
-}
-
-$("#buckaroo-idin-verify-button").click(() => {
-  if ($("#buckaroo-idin-issuer") && ($("#buckaroo-idin-issuer").val().length > 1)) {
-    buckarooIdentify($("#buckaroo-idin-issuer").val());
-  } else {
-    buckarooDisplayErrorMessage('You need to select your bank!');
+        buckarooDisplayErrorMessage("Something went wrong while processing your identification.");
+        $('.woocommerce-checkout').unblock();
+      })
+      .fail(() => {
+        buckarooDisplayErrorMessage("Something went wrong while processing your identification.");
+        $('.woocommerce-checkout').unblock();
+      });
+    },1000);
   }
-});
+
+  function buckarooDisplayErrorMessage(message) {
+    const content = `      
+        <div class="woocommerce-error" role="alert">
+          ${message}
+        </div>
+      `;
+    $('.woocommerce-notices-wrapper').first().prepend(content);
+    var wooError = $('.woocommerce-notices-wrapper .woocommerce-error').first();
+    setTimeout(function(){
+      wooError.fadeOut(1000);
+    }, 10000);
+    $('html, body').scrollTop(0);
+  }
+
+  $("#buckaroo-idin-verify-button").click(() => {
+    if ($("#buckaroo-idin-issuer") && ($("#buckaroo-idin-issuer").val().length > 1)) {
+      buckarooIdentify($("#buckaroo-idin-issuer").val());
+    } else {
+      buckarooDisplayErrorMessage('You need to select your bank!');
+
+    }
+  });
+
+})(jQuery);
