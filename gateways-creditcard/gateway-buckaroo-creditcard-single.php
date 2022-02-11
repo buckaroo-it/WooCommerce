6b04519460a733fc8a2ff21aa8519c17b35babc9
Ivascu Madalin <madalin.ivascu@arnia.ro>
2022-02-11 10:56:42 +0200

    remove unecesary functions

--- gateways-creditcard/gateway-buckaroo-creditcard-single.php
+++ gateways-creditcard/gateway-buckaroo-creditcard-single.php
@@ -13,14 +13,6 @@
  */
 class Buckaroo_Creditcard_Single extends WC_Gateway_Buckaroo_Creditcard
 {
-    public function get_option($key, $empty_value = null)
-    {
-        $value = parent::get_option($key, $empty_value);
-        if ($key === 'title' && $value === 'Creditcards') {
-            return $this->title;
-        }
-        return $value;
-    }
     /**
      * Payment form on checkout page
      *
@@ -43,37 +35,5 @@ class Buckaroo_Creditcard_Single extends WC_Gateway_Buckaroo_Creditcard
             $this->form_fields['AllowedProvider'],
             $this->form_fields[self::SHOW_IN_CHECKOUT_FIELD]
         );
-    }
-    /**
-     * Show or hide creditcard
-     *
-     * @param boolean $show
-     *
-     * @return array
-     */
-    public function updateList($show)
-    {
-        $credit_settings = get_option('woocommerce_buckaroo_creditcard_settings', null);
-
-        if(
-            $credit_settings === null ||
-            !isset($credit_settings['show_in_checkout']) ||
-            !is_array($credit_settings['show_in_checkout'])
-            ) {
-            return false;
-        }
-
-        $list = $credit_settings['show_in_checkout'];
-        $creditcardMethod = str_replace("buckaroo_creditcard_", "", $this->id);
-        if(in_array($creditcardMethod, $list)) {
-            $list = array_diff($list, [$creditcardMethod]);
-        }
-        if($show) {
-            $list[] =  $creditcardMethod;
-        }
-
-        $credit_settings['show_in_checkout'] = $list;
-
-        return update_option('woocommerce_buckaroo_creditcard_settings', $credit_settings);
     }
 }
