3b567009284d8fe17206f6e40fe4679178441e5f
Shu Chen <s.chen@buckaroo.nl>
2022-06-08 13:01:48 +0200

    Update esc_html to esc_url for image urls

--- templates/idin/product.php
+++ templates/idin/product.php
@@ -4,7 +4,7 @@ require_once(dirname(__FILE__) . '/../../library/api/idin.php');
 <div id="buckaroo_idin_cart" class="buckaroo-idin-cart form-row">
     <fieldset>
         <div>
-            <img class="buckaroo_idin_logo" src="<?php echo esc_html(plugin_dir_url(__DIR__) . '../library/buckaroo_images/idin_logo.svg'); ?>" />
+            <img class="buckaroo_idin_logo" src="<?php echo esc_url(plugin_dir_url(__DIR__) . '../library/buckaroo_images/idin_logo.svg'); ?>" />
             <p class="buckaroo_idin_prompt">
                 <?php
                     esc_html_e('You must be 18 years or older to order this product', 'wc-buckaroo-bpe-gateway');
