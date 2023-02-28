2a40a072252d489429f8e81c6c4ebf1749cb7c35
Ivascu Madalin <madalinivascu@gmail.com>
2023-02-28 09:35:43 +0200

    BP-2315 Apple Pay error: Something went wrong while processing your payment (#145)
    Co-authored-by: Ivascu Madalin <madalin.ivascu@arnia.ro>

--- assets/js/webpack.config.js
+++ assets/js/webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 
 const applepay =  {
   entry: './applepay/index.js',
-  mode: 'production',
   output: {
     filename: './apple-pay.js',
     path: path.resolve(__dirname, 'dist'),
@@ -11,7 +10,6 @@ const applepay =  {
 
 const checkout =  {
   entry: './checkout/index.js',
-  mode: 'production',
   output: {
     filename: './checkout.js',
     path: path.resolve(__dirname, 'dist'),
