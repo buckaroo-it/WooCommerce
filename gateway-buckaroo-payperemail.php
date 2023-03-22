0224087e5bbc6279dfd09061a74345ea4ed201f0
Ivascu Madalin <madalinivascu@gmail.com>
2023-03-22 10:42:00 +0200

    BP-1764 Minor translation changes (ENG). (#148)
    Co-authored-by: Ivascu Madalin <madalin.ivascu@arnia.ro>

--- gateway-buckaroo-payperemail.php
+++ gateway-buckaroo-payperemail.php
@@ -188,7 +188,7 @@ class WC_Gateway_Buckaroo_PayPerEmail extends WC_Gateway_Buckaroo
                 'belfius'            => 'Belfius',
                 'Przelewy24'         => 'P24',
             ),
-            'description' => __('select which methods will be appear to customer', 'wc-buckaroo-bpe-gateway'),
+            'description' => __('Select which methods appear to the customer', 'wc-buckaroo-bpe-gateway'),
             'default'     => array('amex', 'cartebancaire', 'cartebleuevisa', 'dankort', 'mastercard', 'postepay', 'visa', 'visaelectron', 'vpay', 'maestro', 'bancontactmrcash', 'transfer', 'giftcard', 'giropay', 'ideal', 'paypal', 'sepadirectdebit', 'sofortueberweisung', 'belfius', 'Przelewy24'),
         );
     }
