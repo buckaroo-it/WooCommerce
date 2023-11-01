5ca5c6fef6b96f4d961968a0c44df58f12379975
Ivascu Madalin <madalin.ivascu@arnia.ro>
2023-11-01 11:35:58 +0000

    v3.10.0

--- library/config.php
+++ library/config.php
@@ -7,7 +7,7 @@ require_once dirname(__FILE__).'/api/config/configcore.php';
 class BuckarooConfig extends BuckarooConfigCore {
     const NAME = 'buckaroo3';
     const PLUGIN_NAME = 'Buckaroo BPE 3.0 official plugin';
-    const VERSION = '3.9.0';
+    const VERSION = '3.10.0';
     const SHIPPING_SKU = "WC8888";
 
     private static $idinCategories;
