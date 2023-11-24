8279a8f71b4894d75f68d90d7467a0bb636e6e19
Ivascu Madalin <madalinivascu@gmail.com>
2023-11-24 15:10:28 +0200

    BP-3146 Add a option to not show the iDEAL issuers selection in the checkout (#192)
    * BP-3146 Add a option to not show the iDEAL issuers selection in the checkout
    
    * added translations
    
    * Apply logo changes from media repository
    
    * fix in3 pipe
    
    * remove empty line
    
    ---------
    
    Co-authored-by: Ivascu Madalin <madalin.ivascu@arnia.ro>
    Co-authored-by: harli91 <harli91@users.noreply.github.com>

--- gateway-buckaroo-ideal.php
+++ gateway-buckaroo-ideal.php
@@ -42,13 +42,15 @@ class WC_Gateway_Buckaroo_Ideal extends WC_Gateway_Buckaroo
      */
     public function validate_fields()
     {
-        $issuer = $this->request('buckaroo-ideal-issuer');
-
-        if ($issuer === null) {
-            wc_add_notice(__("<strong>iDEAL bank </strong> is a required field.", 'wc-buckaroo-bpe-gateway'), 'error');
-        } else
-        if (!in_array($issuer, array_keys(BuckarooIDeal::getIssuerList()))) {
-            wc_add_notice(__("A valid iDEAL bank is required.", 'wc-buckaroo-bpe-gateway'), 'error');
+        if ($this->canShowIssuers()) {
+            $issuer = $this->request('buckaroo-ideal-issuer');
+    
+            if ($issuer === null) {
+                wc_add_notice(__("<strong>iDEAL bank </strong> is a required field.", 'wc-buckaroo-bpe-gateway'), 'error');
+            } else
+            if (!in_array($issuer, array_keys(BuckarooIDeal::getIssuerList()))) {
+                wc_add_notice(__("A valid iDEAL bank is required.", 'wc-buckaroo-bpe-gateway'), 'error');
+            }
         }
         parent::validate_fields();
     }
@@ -64,7 +66,10 @@ class WC_Gateway_Buckaroo_Ideal extends WC_Gateway_Buckaroo
         $order = getWCOrder($order_id);
         /** @var BuckarooIDeal */
         $ideal = $this->createDebitRequest($order);
-        $ideal->issuer = $this->request('buckaroo-ideal-issuer');
+
+        if ($this->canShowIssuers()) {
+            $ideal->issuer = $this->request('buckaroo-ideal-issuer');
+        }
 
         $response = $this->apply_filters_or_error('buckaroo_before_payment_request', $order, $ideal);
         if ($response) {
@@ -73,5 +78,30 @@ class WC_Gateway_Buckaroo_Ideal extends WC_Gateway_Buckaroo
 
         $response = $ideal->Pay();
         return fn_buckaroo_process_response($this, $response);
+    }
+
+    public function canShowIssuers() {
+        return $this->get_option('show_issuers') !== 'no';
+    }
+
+     /**
+     * Add fields to the form_fields() array, specific to this page.
+     *
+     * @access public
+     */
+    public function init_form_fields()
+    {
+        parent::init_form_fields();
+
+        $this->form_fields['show_issuers'] = array(
+            'title'       => __('Show Issuer Selection in the Checkout', 'wc-buckaroo-bpe-gateway'),
+            'type'        => 'select',
+            'description' => __('When the "NO" option is selected, the issuer selection for iDEAL will not be displayed in the checkout. Instead, customers will be redirected to a separate page where they can choose their iDEAL issuer (i.e., their bank). On the other hand, selecting the "Yes" option will display the issuer selection directly in the checkout. It\'s important to note that enabling this option will incur additional costs from Buckaroo, estimated at around €0.002 for each transaction. For precise cost details, please reach out to <a href="mailto:wecare@buckaroo.nl">Buckaroo</a> directly.', 'wc-buckaroo-bpe-gateway'),
+            'options'     => array(
+                'yes' => __('Yes'),
+                'no' => __('No'),
+            ),
+            'default'     => 'yes'
+        );
     }
 }
