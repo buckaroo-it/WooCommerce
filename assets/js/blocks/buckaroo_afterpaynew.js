5e554a7dbe216062f71bcd1bdd27cc5a68e91014
gentiprenaj <gentianprenaj@gmail.com>
2024-01-09 14:24:57 +0100

    afterpay (new)

---
+++ assets/js/blocks/buckaroo_afterpaynew.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import BirthDayField from './partials/buckaroo_partial_birth_field'
+import FinancialWarning from "./partials/buckaroo_financial_warning";
+import AfterPayB2B from "./partials/buckaroo_afterpay_b2b";
+
+const AfterPayNew = () => {
+    const paymentMethod = 'buckaroo-afterpaynew';
+
+    return (
+        <div>
+            <BirthDayField sectionId={paymentMethod} />
+            <AfterPayB2B field={paymentMethod}></AfterPayB2B>
+            <FinancialWarning paymentMethod={paymentMethod} />
+        </div>
+    );
+
+};
+
+export default AfterPayNew;
