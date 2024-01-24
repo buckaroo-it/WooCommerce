916eff817971c1145f2d17d24d94eb1a5b1e5def
v.carkaxhija <vegim.carkaxhija@gmail.com>
2024-01-24 15:22:09 +0100

    fix paybybank

--- assets/js/blocks/gateways/buckaroo_paybybank.js
+++ assets/js/blocks/gateways/buckaroo_paybybank.js
@@ -31,35 +31,51 @@ const PayByBankDropdown = ({ payByBankIssuers, onSelectIssuer, buckarooImagesUrl
                     </select>
                 </div>
             ) : (
-                // Radio Buttons for Desktop View
-                <div className="bk-paybybank-input bk-paybybank-not-mobile">
-                    <div className={`form-row form-row-wide bk-paybybank-selector ${showAllBanks ? 'show-all' : ''}`}>
-                        {Object.keys(payByBankIssuers).map((issuerCode) => (
-                            <div className="custom-control custom-radio bank-control" key={issuerCode}>
-                                <input
-                                    type="radio"
-                                    id={`radio-bankMethod-${issuerCode}`}
-                                    name="buckaroo-paybybank-radio-issuer"
-                                    value={issuerCode}
-                                    checked={selectedIssuer === issuerCode}
-                                    onChange={() => handleSelectIssuer(issuerCode)}
-                                    className="custom-control-input bank-method-input bk-paybybank-radio"
-                                />
-                                <label className="custom-control-label bank-method-label" htmlFor={`radio-bankMethod-${issuerCode}`}>
-                                    <img src={buckarooImagesUrl + 'ideal/' +(payByBankIssuers[issuerCode].logo || 'default-logo-filename.png')} className="bank-method-image" alt={payByBankIssuers[issuerCode].name} />
-                                    <strong>{payByBankIssuers[issuerCode].name}</strong>
-                                </label>
-                            </div>
-                        ))}
+                <div>
+                    <div className="form-row form-row-wide bk-paybybank-input bk-paybybank-mobile" style={{ display: 'none' }}>
+                        <select className="buckaroo-custom-select" value={selectedIssuer}
+                                onChange={(e) => handleSelectIssuer(e.target.value)}>
+                            <option value="0">Select your bank</option>
+                            {Object.keys(payByBankIssuers).map((issuerCode) => (
+                                <option key={issuerCode} value={issuerCode}>
+                                    {payByBankIssuers[issuerCode].name}
+                                </option>
+                            ))}
+                        </select>
                     </div>
+                    <div className="bk-paybybank-input bk-paybybank-not-mobile">
+                        <div
+                            className={`form-row form-row-wide bk-paybybank-selector ${showAllBanks ? 'show-all' : ''}`}>
+                            {Object.keys(payByBankIssuers).map((issuerCode) => (
+                                <div className="custom-control custom-radio bank-control" key={issuerCode}>
+                                    <input
+                                        type="radio"
+                                        id={`radio-bankMethod-${issuerCode}`}
+                                        name="buckaroo-paybybank-radio-issuer"
+                                        value={issuerCode}
+                                        checked={selectedIssuer === issuerCode}
+                                        onChange={() => handleSelectIssuer(issuerCode)}
+                                        className="custom-control-input bank-method-input bk-paybybank-radio"
+                                    />
+                                    <label className="custom-control-label bank-method-label"
+                                           htmlFor={`radio-bankMethod-${issuerCode}`}>
+                                        <img
+                                            src={buckarooImagesUrl + 'ideal/' + (payByBankIssuers[issuerCode].logo || 'default-logo-filename.png')}
+                                            className="bank-method-image" alt={payByBankIssuers[issuerCode].name}/>
+                                        <strong>{payByBankIssuers[issuerCode].name}</strong>
+                                    </label>
+                                </div>
+                            ))}
+                        </div>
 
-                    {/* Toggle Button */}
-                    <div className="bk-paybybank-toggle-list" onClick={toggleBankView}>
-                        <div className="bk-toggle-wrap">
-                            <div className="bk-toggle-text">
-                                {showAllBanks ? 'More banks' : 'Less banks'}
+                        {/* Toggle Button */}
+                        <div className="bk-paybybank-toggle-list" onClick={toggleBankView}>
+                            <div className="bk-toggle-wrap">
+                                <div className="bk-toggle-text">
+                                    {showAllBanks ? 'More banks' : 'Less banks'}
+                                </div>
+                                <div className={`bk-toggle ${showAllBanks ? 'bk-toggle-up' : 'bk-toggle-down'}`}></div>
                             </div>
-                            <div className={`bk-toggle ${showAllBanks ? 'bk-toggle-up' : 'bk-toggle-down'}`}></div>
                         </div>
                     </div>
                 </div>
