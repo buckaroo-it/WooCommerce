44eaa064346694fe6cd91f6954e997320fa45034
gentiprenaj <gentianprenaj@gmail.com>
2024-02-06 14:51:30 +0100

    CODE_CANCELLED_BY_USER changed to status on hold

--- library/api/abstract.php
+++ library/api/abstract.php
@@ -68,7 +68,7 @@ abstract class BuckarooAbstract {
         ),
         self::CODE_CANCELLED_BY_USER => array(
             'message' => 'Cancelled by consumer',
-            'status'  => self::STATUS_CANCELED,
+            'status'  => self::STATUS_ON_HOLD,
         ),
         self::CODE_CANCELLED_BY_MERCHANT => array(
             'message' => 'Cancelled by merchant',
