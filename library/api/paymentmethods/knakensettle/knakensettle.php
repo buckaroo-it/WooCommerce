223145ed2107bddb6ac0ec097b1ec665d61e9176
a.baraliu <baraliualbina@gmail.com>
2024-02-21 13:48:55 +0100

    added knaken as a payment method

---
+++ library/api/paymentmethods/knakensettle/knakensettle.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once(dirname(__FILE__) . '/../paymentmethod.php');
+
+class BuckarooKnakenSettle extends BuckarooPaymentMethod {
+    public function __construct() {
+        $this->type = "knaken";
+        $this->version = 1;
+    }
+
+    /**
+     * @access public
+     * @param array $customVars
+     * @return callable parent::Pay()
+     */
+    public function Pay($customVars = array())
+    {
+        return parent::Pay();
+    }
+}
