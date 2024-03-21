22dd47442fc5d270399fc8ba4afebfad639dbd2e
Ivascu Madalin <madalin.ivascu@buckaroo.nl>
2024-03-21 08:32:12 +0000

    run build

--- assets/js/dist/blocks.asset.php
+++ assets/js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '4f836ef0be00bc3de69b');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '2b8111e6785179409db2');
