85b467131a1ef123c3856ed0222176dad381b3f3
Ivascu Madalin <madalin.ivascu@buckaroo.nl>
2024-04-02 09:18:12 +0000

    keep issuers

--- gateway-buckaroo-ideal.php
+++ gateway-buckaroo-ideal.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once(dirname(__FILE__) . '/library/api/paymentmethods/ideal/ideal.php');
 
 /**
  * @package Buckaroo
--- gateway-buckaroo-paybybank.php
+++ gateway-buckaroo-paybybank.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once(dirname(__FILE__) . '/library/api/paymentmethods/paybybank/paybybank.php');
 
 /**
  * @package Buckaroo
