fb4c6515f25de19267bc00eb4bd241bf3801600e
Vildan Bina <vildanbina@gmail.com>
2024-10-13 20:55:40 +0200

    add response parser classes and integrate with processors

--- src/Gateways/AbstractPaymentProcessor.php
+++ src/Gateways/AbstractPaymentProcessor.php
@@ -7,34 +7,8 @@ use Buckaroo\Woocommerce\Components\OrderDetails;
 use Buckaroo\Woocommerce\Services\HttpRequest;
 use WC_Order;
 
-class AbstractPaymentProcessor extends AbstractPaymentHelper
+class AbstractPaymentProcessor extends AbstractProcessor
 {
-    const TYPE_PAY = 'pay';
-    const TYPE_CAPTURE = 'capture';
-    const TYPE_REFUND = 'refund';
-    const VERSION_ZERO = 0;
-    const VERSION_ONE = 1;
-    const VERSION_TWO = 2;
-    public const REQUEST_TYPE_DATA_REQUEST = 'DataRequest';
-    public $currency;
-    public $amountDedit;
-    public $amountCredit = 0;
-    public $orderId;
-    public $invoiceId;
-    public $description;
-    public $OriginalTransactionKey;
-    public $OriginalInvoiceNumber;
-    public $AmountVat;
-    public $returnUrl;
-    public $mode;
-    public $version;
-    public $sellerprotection = 0;
-    public $CreditCardDataEncrypted;
-    public $real_order_id;
-    public AbstractPaymentGateway $gateway;
-    protected $type;
-    protected $data = array();
-    protected $requestType = 'TransactionRequest';
     protected OrderDetails $order_details;
     protected OrderArticles $order_articles;
     private HttpRequest $request;
@@ -50,49 +24,6 @@ class AbstractPaymentProcessor extends AbstractPaymentHelper
         $this->request = $request;
         $this->order_details = $order_details;
         $this->order_articles = $order_articles;
-    }
-
-    /**
-     * Calculate checksum from iban and confirm validity of iban
-     *
-     * @access public
-     * @param string $iban
-     * @return boolean
-     */
-    public static function isIBAN($iban)
-    {
-        // Normalize input (remove spaces and make upcase)
-        $iban = strtoupper(str_replace(' ', '', $iban));
-
-        if (preg_match('/^[A-Z]{2}[0-9]{2}[A-Z0-9]{1,30}$/', $iban)) {
-            $country = substr($iban, 0, 2);
-            $check = intval(substr($iban, 2, 2));
-            $account = substr($iban, 4);
-
-            // To numeric representation
-            $search = range('A', 'Z');
-            foreach (range(10, 35) as $tmp) {
-                $replace[] = strval($tmp);
-            }
-            $numstr = str_replace($search, $replace, $account . $country . '00');
-
-            // Calculate checksum
-            $checksum = intval(substr($numstr, 0, 1));
-            for ($pos = 1; $pos < strlen($numstr); $pos++) {
-                $checksum *= 10;
-                $checksum += intval(substr($numstr, $pos, 1));
-                $checksum %= 97;
-            }
-
-            return ((98 - $checksum) == $check);
-        } else {
-            return false;
-        }
-    }
-
-    public function setRequestType(string $type)
-    {
-        $this->requestType = $type;
     }
 
     public function getAction(): string
@@ -117,14 +48,14 @@ class AbstractPaymentProcessor extends AbstractPaymentHelper
                 ],
 
                 'description' => $this->get_description(),
-                'clientIP' => $this->get_ip(),
+                'clientIP' => $this->getIp(),
             ]
         );
     }
 
     protected function getMethodBody(): array
     {
-        return array();
+        return [];
     }
 
     /**
@@ -183,100 +114,6 @@ class AbstractPaymentProcessor extends AbstractPaymentHelper
     }
 
     /**
-     * Get ip
-     *
-     * @return string
-     */
-    protected function get_ip(): string
-    {
-        $ipaddress = '';
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) {
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        } elseif (!empty($_SERVER['HTTP_FORWARDED_FOR'])) {
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        } elseif (!empty($_SERVER['HTTP_FORWARDED'])) {
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
-        } else {
-            $ipaddress = 'UNKNOWN';
-        }
-        $ex = explode(",", sanitize_text_field($ipaddress));
-        if (filter_var($ex[0], FILTER_VALIDATE_IP)) {
-            return trim($ex[0]);
-        }
-        return "";
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getType()
-    {
-        return $this->type;
-    }
-
-    /**
-     * @param mixed $type
-     */
-    public function setType($type)
-    {
-        $this->type = $type;
-    }
-
-    /**
-     * Get real order id
-     *
-     * @return int
-     */
-    public function getRealOrderId()
-    {
-        if (is_int($this->real_order_id)) {
-            return $this->real_order_id;
-        }
-        return $this->orderId;
-    }
-
-    public function order_number_shortcode()
-    {
-        return $this->data['description'] . ' ' . $this->invoiceId;
-    }
-
-    /**
-     * Get ip
-     *
-     * @return string
-     */
-    protected function getIp(): string
-    {
-        $ipaddress = '';
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED'])) {
-            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
-        } elseif (!empty($_SERVER['HTTP_FORWARDED_FOR'])) {
-            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
-        } elseif (!empty($_SERVER['HTTP_FORWARDED'])) {
-            $ipaddress = $_SERVER['HTTP_FORWARDED'];
-        } elseif (!empty($_SERVER['REMOTE_ADDR'])) {
-            $ipaddress = $_SERVER['REMOTE_ADDR'];
-        } else {
-            $ipaddress = 'UNKNOWN';
-        }
-        $ex = explode(",", sanitize_text_field($ipaddress));
-        if (filter_var($ex[0], FILTER_VALIDATE_IP)) {
-            return trim($ex[0]);
-        }
-        return "";
-    }
-
-    /**
      * Get address component
      *
      * @param string $type
@@ -313,15 +150,5 @@ class AbstractPaymentProcessor extends AbstractPaymentHelper
     {
         $value = $this->request->request($key);
         return $value ?? $default;
-    }
-
-    /**
-     * Get invoice number for refund
-     *
-     * @return string
-     */
-    private function getInvoiceNumber()
-    {
-        return $this->invoiceId;
     }
 }
