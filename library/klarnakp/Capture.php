127e0cbf8b0219ae1e1f11f739cbfb52a79ee881
v.carkaxhija <vegim.carkaxhija@gmail.com>
2024-10-15 14:25:38 +0200

    change Capture.php

--- library/klarnakp/Capture.php
+++ library/klarnakp/Capture.php
@@ -13,76 +13,82 @@
  * @link      https://www.buckaroo.eu/
  */
 
-class Buckaroo_Capture_Form {
+class Buckaroo_Capture_Form
+{
 
+    public function __construct()
+    {
+        add_action('add_meta_boxes', array($this, 'add_meta_box_form'), 10, 2);
+    }
 
-	public function __construct() {
-		add_action( 'add_meta_boxes_shop_order', array( $this, 'add_meta_box_form' ) );
-	}
+    public function output($order)
+    {
+        $order_capture = new Buckaroo_Order_Capture(
+            new Buckaroo_Order_Details( $order ),
+            new Buckaroo_Http_Request()
+        );
+        include 'capture-form.php';
+    }
 
-	public function output( WP_POST $post ) {
+    public function add_meta_box_form($post_type, $order)
+    {
+        if ($post_type != 'woocommerce_page_wc-orders') {
+            return;
+        }
 
-		$order = wc_get_order( $post->ID );
+        if (
+            $order->get_payment_method() === 'buckaroo_klarnakp' &&
+            get_post_meta($order->get_id(), 'buckaroo_is_reserved', true) === 'yes'
+        ) {
+            add_meta_box(
+                'buckaroo-order-klarnakp-capture',
+                __('Capture & refund order', 'woocommerce'),
+                array($this, 'output'),
+                'woocommerce_page_wc-orders',
+                'normal',
+                'default'
+            );
+        }
+    }
 
-		$order_capture = new Buckaroo_Order_Capture(
-			new Buckaroo_Order_Details( $order ),
-			new Buckaroo_Http_Request()
-		);
-		include 'capture-form.php';
-	}
+    /**
+     * Get items available to capture by type
+     *
+     * @param Buckaroo_Order_Capture $order_capture
+     *
+     * @return array
+     */
+    protected function get_available_to_capture_by_type(Buckaroo_Order_Capture $order_capture)
+    {
+        $available_to_capture = $order_capture->get_available_to_capture();
 
-	public function add_meta_box_form( $post ) {
-		$order = wc_get_order( $post->ID );
-		if (
-			$order->get_payment_method() === 'buckaroo_klarnakp' &&
-			get_post_meta( $order->get_id(), 'bukaroo_is_reserved', true ) === 'yes'
-		) {
-			add_meta_box(
-				'buckaroo-order-klarnakp-capture',
-				__( 'Capture & refund order', 'woocommerce' ),
-				array( $this, 'output' ),
-				'shop_order',
-				'normal',
-				'low'
-			);
-		}
-	}
+        $available_to_capture_by_type = array();
+        foreach ($available_to_capture as $item) {
+            $item_type = $item->get_type();
+            if (!isset($available_to_capture_by_type[$item_type])) {
+                $available_to_capture_by_type[$item_type] = array();
+            }
+            $available_to_capture_by_type[$item_type][] = $item;
+        }
+        return $available_to_capture_by_type;
+    }
 
-	/**
-	 * Get items available to capture by type
-	 *
-	 * @param Buckaroo_Order_Capture $order_capture
-	 *
-	 * @return array
-	 */
-	protected function get_available_to_capture_by_type( Buckaroo_Order_Capture $order_capture ) {
-		$available_to_capture = $order_capture->get_available_to_capture();
-
-		$available_to_capture_by_type = array();
-		foreach ( $available_to_capture as $item ) {
-			$item_type = $item->get_type();
-			if ( ! isset( $available_to_capture_by_type[ $item_type ] ) ) {
-				$available_to_capture_by_type[ $item_type ] = array();
-			}
-			$available_to_capture_by_type[ $item_type ][] = $item;
-		}
-		return $available_to_capture_by_type;
-	}
-	/**
-	 * Get refunded captures for $order_id
-	 *
-	 * @param integer $order_id
-	 *
-	 * @return array
-	 */
-	protected function get_refunded_captures( int $order_id ) {
-		$refunded_captures = get_post_meta( $order_id, 'buckaroo_captures_refunded', true );
-		if ( is_string( $refunded_captures ) ) {
-			$refunded_captures_decoded = json_decode( $refunded_captures );
-			if ( is_array( $refunded_captures_decoded ) ) {
-				return $refunded_captures_decoded;
-			}
-		}
-		return array();
-	}
+    /**
+     * Get refunded captures for $order_id
+     *
+     * @param integer $order_id
+     *
+     * @return array
+     */
+    protected function get_refunded_captures(int $order_id)
+    {
+        $refunded_captures = get_post_meta($order_id, 'buckaroo_captures_refunded', true);
+        if (is_string($refunded_captures)) {
+            $refunded_captures_decoded = json_decode($refunded_captures);
+            if (is_array($refunded_captures_decoded)) {
+                return $refunded_captures_decoded;
+            }
+        }
+        return array();
+    }
 }
