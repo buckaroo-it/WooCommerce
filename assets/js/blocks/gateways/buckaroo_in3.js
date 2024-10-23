5e53e39b323c71d21a5b5f9335bc2f7ccc83a039
Vildan Bina <vildanbina@gmail.com>
2024-10-23 11:36:25 +0200

    add latest PRs from develop

--- assets/js/blocks/gateways/buckaroo_in3.js
+++ assets/js/blocks/gateways/buckaroo_in3.js
@@ -4,31 +4,32 @@ import FinancialWarning from '../partials/buckaroo_financial_warning';
 import useFormData from '../hooks/useFormData';
 import PhoneDropdown from '../partials/buckaroo_phone';
 
-function In3({ onStateChange, methodName, billing }) {
-  const initialState = {
-    [`${methodName}-phone`]: billing?.phone || '',
-    [`${methodName}-birthdate`]: '',
-  };
+function In3({onStateChange, methodName, title, billing}) {
+    const initialState = {
+        [`${methodName}-phone`]: billing?.phone || '',
+        [`${methodName}-birthdate`]: '',
+    };
 
-  const { formState, updateFormState } = useFormData(initialState, onStateChange);
+    const {formState, updateFormState} = useFormData(initialState, onStateChange);
 
-  const handlePhoneChange = (value) => {
-    updateFormState(`${methodName}-phone`, value);
-  };
+    const handlePhoneChange = (value) => {
+        updateFormState(`${methodName}-phone`, value);
+    };
 
-  const handleBirthDayChange = (value) => {
-    updateFormState(`${methodName}-birthdate`, value);
-  };
+    const handleBirthDayChange = (value) => {
+        updateFormState(`${methodName}-birthdate`, value);
+    };
 
-  return (
-    <div>
-      {billing.country === 'NL'
-                && <BirthDayField paymentMethod={methodName} handleBirthDayChange={handleBirthDayChange} />}
-      {billing.phone === ''
-                && <PhoneDropdown paymentMethod={methodName} formState={formState} handlePhoneChange={handlePhoneChange} />}
-      <FinancialWarning paymentMethod={methodName} />
-    </div>
-  );
+    return (
+        <div>
+            {billing.country === 'NL'
+                && <BirthDayField paymentMethod={methodName} handleBirthDayChange={handleBirthDayChange}/>}
+            {billing.phone === ''
+                &&
+                <PhoneDropdown paymentMethod={methodName} formState={formState} handlePhoneChange={handlePhoneChange}/>}
+            <FinancialWarning title={title}/>
+        </div>
+    );
 }
 
 export default In3;
