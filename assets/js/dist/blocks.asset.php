ef3002339737065dee5e861fb4711ecf59d13a64
Vildan Bina <vildanbina@gmail.com>
2024-10-23 11:36:25 +0200

    add latest PRs from develop

--- assets/js/dist/blocks.asset.php
+++ assets/js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'cf058b262a8f049186a3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '5fb613376ce4f6aaec84');
