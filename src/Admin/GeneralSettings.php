7493d0c8e7ee79626cc415648dcb96d1704fbdb3
Vildan Bina <vildanbina@gmail.com>
2024-10-29 15:50:11 +0100

    fix translations

--- src/Admin/GeneralSettings.php
+++ src/Admin/GeneralSettings.php
@@ -122,9 +122,7 @@ class GeneralSettings extends WC_Settings_Page
                     'type' => 'title',
                     'id' => 'buckaroo-page',
                     'desc' => __(
-                        'Integrate more then 30+ international payment methods in your WooCommerce webshop. Simply enable them into your WooCommerce webshop with the Buckaroo Payments plugin.
-                        Please go to the <a href="https://plaza.buckaroo.nl/Configuration/Website/Index/">signup page</a> to create a Buckaroo account and start receiving payments.</br>
-                        Contact <a href="mailto:support@buckaroo.nl">support@buckaroo.nl</a> if you have any questions about this plugin.',
+                        'Integrate more then 30+ international payment methods in your WooCommerce webshop. Simply enable them into your WooCommerce webshop with the Buckaroo Payments plugin.</br>Please go to the <a href="https://plaza.buckaroo.nl/Configuration/Website/Index/">signup page</a> to create a Buckaroo account and start receiving payments.</br>Contact <a href="mailto:support@buckaroo.nl">support@buckaroo.nl</a> if you have any questions about this plugin.',
                         'wc-buckaroo-bpe-gateway'
                     ),
                 ),
