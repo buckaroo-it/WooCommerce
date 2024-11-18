101768c3ab4d70fe4efac6dd589551e3070ec1c1
Sander <sander.vdhulst98@gmail.com>
2024-11-18 13:49:02 +0100

    BP-3907-Issuer list not displayed correctly in legacy checkout on latest Chrome version

--- templates/gateways/ideal.php
+++ templates/gateways/ideal.php
@@ -1,1 +1,35 @@
-<?php/** * The Template for displaying ideal gateway template * php version 7.2 * * @category  Payment_Gateways * @package   Buckaroo * @author    Buckaroo <support@buckaroo.nl> * @copyright 2021 Copyright (c) Buckaroo B.V. * @license   MIT https://tldrlegal.com/license/mit-license * @version   GIT: 2.25.0 * @link      https://www.buckaroo.eu/ */defined( 'ABSPATH' ) || exit;if ( $this->canShowIssuers() ) {	?>	<div style="background: none">		<p class="form-row form-row-wide">			<select name="buckaroo-ideal-issuer" id="buckaroo-ideal-issuer">				<option value="0" style="color: grey !important">					<?php echo esc_html_e( 'Select your bank', 'wc-buckaroo-bpe-gateway' ); ?>				</option>				<?php foreach ( BuckarooIDeal::getIssuerList() as $key => $issuer ) : ?>                    <option value="<?php echo esc_attr( $key ); ?>">                        <?php echo esc_html_e( $issuer['name'], 'wc-buckaroo-bpe-gateway' ); ?>                    </option>				<?php endforeach ?>			</select>		</p>	</div>	<?php}
+<?php
+
+/**
+ * The Template for displaying ideal gateway template
+ * php version 7.2
+ *
+ * @category  Payment_Gateways
+ * @package   Buckaroo
+ * @author    Buckaroo <support@buckaroo.nl>
+ * @copyright 2021 Copyright (c) Buckaroo B.V.
+ * @license   MIT https://tldrlegal.com/license/mit-license
+ * @version   GIT: 2.25.0
+ * @link      https://www.buckaroo.eu/
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( $this->canShowIssuers() ) {
+	?>
+	<div style="background: none">
+		<p class="form-row form-row-wide">
+			<select name="buckaroo-ideal-issuer" id="buckaroo-ideal-issuer">
+				<option value="0" style="color: grey !important">
+					<?php echo esc_html_e( 'Select your bank', 'wc-buckaroo-bpe-gateway' ); ?>
+				</option>
+				<?php foreach ( BuckarooIDeal::getIssuerList() as $key => $issuer ) : ?>
+                    <option value="<?php echo esc_attr( $key ); ?>">
+                        <?php echo esc_html_e( $issuer['name'], 'wc-buckaroo-bpe-gateway' ); ?>
+                    </option>
+				<?php endforeach ?>
+			</select>
+		</p>
+	</div>
+	<?php
+}
