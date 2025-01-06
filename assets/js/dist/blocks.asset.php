d0bf3792afe8993ca842578f3196ab334c7fea14
Vildan Bina <vildanbina@gmail.com>
2025-01-06 15:14:50 +0100

    - add "Required" field translation for Billink - fix NL date format issue

--- assets/js/dist/blocks.asset.php
+++ assets/js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n'), 'version' => '298debb64004e74de351');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n'), 'version' => 'daf1cb5d060562a2891e');
