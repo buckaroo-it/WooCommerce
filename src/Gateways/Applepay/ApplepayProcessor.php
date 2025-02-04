1ca8dcb4da7699dff66865698d01dc546aaf6f74
Vildan Bina <vildanbina@gmail.com>
2025-02-04 16:43:00 +0100

    fix: code formatting

--- src/Gateways/Applepay/ApplepayProcessor.php
+++ src/Gateways/Applepay/ApplepayProcessor.php
@@ -4,17 +4,16 @@ namespace Buckaroo\Woocommerce\Gateways\Applepay;
 
 use Buckaroo\Woocommerce\Gateways\AbstractPaymentProcessor;
 
-class ApplepayProcessor extends AbstractPaymentProcessor
-{
+class ApplepayProcessor extends AbstractPaymentProcessor {
+
 
     protected $data;
 
     /** @inheritDoc */
-    protected function getMethodBody(): array
-    {
+    protected function getMethodBody(): array {
         return array(
-            'customerCardName' => $this->get_customer_name($this->request->input('paymentData')),
-            'paymentData' => $this->get_payment_data($this->request->input('paymentData')),
+            'customerCardName' => $this->get_customer_name( $this->request->input( 'paymentData' ) ),
+            'paymentData'      => $this->get_payment_data( $this->request->input( 'paymentData' ) ),
         );
     }
 
@@ -22,12 +21,11 @@ class ApplepayProcessor extends AbstractPaymentProcessor
      * @param mixed $data
      * @return string
      */
-    private function get_customer_name($data): string
-    {
+    private function get_customer_name( $data ): string {
         if (
-            isset($data['billingContact']) &&
-            isset($data['billingContact']['givenName']) &&
-            isset($data['billingContact']['familyName'])
+            isset( $data['billingContact'] ) &&
+            isset( $data['billingContact']['givenName'] ) &&
+            isset( $data['billingContact']['familyName'] )
         ) {
             return $data['billingContact']['givenName'] . ' ' . $data['billingContact']['familyName'];
         }
@@ -38,12 +36,11 @@ class ApplepayProcessor extends AbstractPaymentProcessor
      * @param mixed $data
      * @return string
      */
-    private function get_payment_data($data): string
-    {
-        if (!isset($data['token']) || empty($data['token'])) {
+    private function get_payment_data( $data ): string {
+        if ( ! isset( $data['token'] ) || empty( $data['token'] ) ) {
             return '';
         }
 
-        return base64_encode(json_encode($data['token']));
+        return base64_encode( json_encode( $data['token'] ) );
     }
 }
