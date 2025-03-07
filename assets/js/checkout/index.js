a76abe63c8a43ad4d2ccf82311df85d0d0222850
Vildan Bina <vildanbina@gmail.com>
2025-03-07 13:08:22 +0100

    Revert "Revert "BP-4190: Hosted Fields Implementation""

--- assets/js/checkout/index.js
+++ assets/js/checkout/index.js
@@ -1,9 +1,9 @@
 import BuckarooCheckout from './checkout';
-import BuckarooValidateCreditCards from './creditcard-call-encryption';
+import BuckarooCreditCardsHostedFields from './creditcard-hosted-fields';
 import BuckarooIdin from './idin';
 
 jQuery( () => {
 	new BuckarooCheckout().listen();
-	new BuckarooValidateCreditCards().listen();
 	new BuckarooIdin().listen();
+	new BuckarooCreditCardsHostedFields().listen();
 } );
