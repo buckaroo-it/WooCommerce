3618b18ce8751196d232fb6115c38d2aa58ebb8f
vildanbina <v.bina@buckaroo-kos.com>
2025-09-02 14:16:02 +0200

    feat: implement ExpressPaymentManager for streamlined express payment handling

--- assets/js/dist/blocks.asset.php
+++ assets/js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n'), 'version' => '3a49a2260833714c533a');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n'), 'version' => 'bbcfca77a878cb268ed3');
