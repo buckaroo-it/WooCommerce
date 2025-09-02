e249333c633fa3d7dc92c7b3be5784d85ef902f9
vildanbina <v.bina@buckaroo-kos.com>
2025-09-02 14:44:15 +0200

    feat: add error handling for Buckaroo payment methods

--- assets/js/blocks/index.js
+++ assets/js/blocks/index.js
@@ -211,3 +211,4 @@ const handleBuckarooErrorDisplay = ({ location, wp }) => {
 
 registerBuckarooPaymentMethods(window);
 registerBuckarooExpressPaymentMethods(window);
+handleBuckarooErrorDisplay(window);
--- assets/js/dist/blocks.asset.php
+++ assets/js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n'), 'version' => 'bbcfca77a878cb268ed3');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-data', 'wp-i18n'), 'version' => 'c62d676b0db90393abdd');
