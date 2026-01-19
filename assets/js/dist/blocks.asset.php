509b7efae0ae5d3a951131258f3a32a31f1fa7be
g.prenaj <gentianprenaj@gmail.com>
2026-01-19 16:12:53 +0100

    fix payment method frontend for new versions

--- assets/js/dist/blocks.asset.php
+++ assets/js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'd92e99c34a1aa799bd31');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'b8217079c058087fa834');
