3ed27b464da94f6d3cd41780ea89f8b6142a53e6
g.prenaj <gentianprenaj@gmail.com>
2026-02-05 10:56:03 +0100

    Ideal co rebranding to ideal - wero

--- src/Gateways/Ideal/IdealGateway.php
+++ src/Gateways/Ideal/IdealGateway.php
@@ -6,18 +6,57 @@ use Buckaroo\Woocommerce\Gateways\AbstractPaymentGateway;
 
 class IdealGateway extends AbstractPaymentGateway
 {
+    private const COBRANDED_LABEL = 'iDEAL | Wero';
+
     public const PAYMENT_CLASS = IdealProcessor::class;
 
     public function __construct()
     {
         $this->id = 'buckaroo_ideal';
-        $this->title = 'iDEAL';
+        $this->title = self::getBrandingDisplayName();
         $this->has_fields = true;
-        $this->method_title = 'Buckaroo iDEAL';
-        $this->setIcon('svg/ideal.svg');
+        $this->method_title = 'Buckaroo ' . self::getBrandingDisplayName();
+        $this->setIcon(self::getBrandingIconPath());
 
         parent::__construct();
         $this->addRefundSupport();
         apply_filters('buckaroo_init_payment_class', $this);
+    }
+
+    public static function getBrandingDisplayName(): string
+    {
+        return self::COBRANDED_LABEL;
+    }
+
+    public static function getBrandingIconPath(): string
+    {
+        return 'svg/ideal-wero.svg';
+    }
+
+    /**
+     * Override default property initialisation to enforce the co-branded
+     * naming in checkout even when legacy settings are stored.
+     */
+    protected function setProperties()
+    {
+        parent::setProperties();
+
+
+        if ($this->title === 'iDEAL') {
+            $this->title = self::getBrandingDisplayName();
+        }
+
+
+        $legacyDescription = sprintf(
+            __('Pay with %s', 'wc-buckaroo-bpe-gateway'),
+            'iDEAL'
+        );
+
+        if ($this->description === $legacyDescription) {
+            $this->description = sprintf(
+                __('Pay with %s', 'wc-buckaroo-bpe-gateway'),
+                $this->title
+            );
+        }
     }
 }
