73e0a326a3cd9ba0dee7fac6c9c5b30a055446ec
g.prenaj <gentianprenaj@gmail.com>
2026-06-16 12:29:07 +0200

    Remove riverty new phone number input blocks and classic checkout

--- assets/js/dist/blocks.asset.php
+++ assets/js/dist/blocks.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => '18e47385d4e861d85aa7');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-i18n'), 'version' => 'd68212aa0f7189e0a9af');
