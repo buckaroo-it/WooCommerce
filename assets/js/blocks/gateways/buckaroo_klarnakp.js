d43019f9e7d01db1d3232ec86bad13bb3d8669cc
g.prenaj <gentianprenaj@gmail.com>
2026-06-22 12:50:00 +0200

    prettier issue fix

--- assets/js/blocks/gateways/buckaroo_klarnakp.js
+++ assets/js/blocks/gateways/buckaroo_klarnakp.js
@@ -4,11 +4,7 @@ import FinancialWarning from '../partials/buckaroo_financial_warning';
 // Gender selection removed from checkout to reduce friction.
 // The processor always sends "Unknown" for the mandatory Klarna gender parameter.
 function KlarnaKp({ title, gateway: { financialWarning } }) {
-    return (
-        <div id="buckaroo_klarnakp">
-            {financialWarning === 'enable' && <FinancialWarning title={title} />}
-        </div>
-    );
+    return <div id="buckaroo_klarnakp">{financialWarning === 'enable' && <FinancialWarning title={title} />}</div>;
 }
 
 export default KlarnaKp;
--- assets/js/blocks/gateways/buckaroo_klarnapay.js
+++ assets/js/blocks/gateways/buckaroo_klarnapay.js
@@ -4,11 +4,7 @@ import FinancialWarning from '../partials/buckaroo_financial_warning';
 // Gender selection removed from checkout to reduce friction.
 // The processor always sends "Unknown" for the mandatory Klarna gender parameter.
 function KlarnaPay({ title, gateway: { financialWarning } }) {
-    return (
-        <div id="buckaroo_klarnapay">
-            {financialWarning === 'enable' && <FinancialWarning title={title} />}
-        </div>
-    );
+    return <div id="buckaroo_klarnapay">{financialWarning === 'enable' && <FinancialWarning title={title} />}</div>;
 }
 
 export default KlarnaPay;
