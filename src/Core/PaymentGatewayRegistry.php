a9ddcf2cbea31f6f819fddccbb71bc3a0ed14e26
Vildan Bina <vildanbina@gmail.com>
2026-09-07 13:37:34 +0200

    feat(BTI-1341): remove goSettle as a payment method

--- src/Core/PaymentGatewayRegistry.php
+++ src/Core/PaymentGatewayRegistry.php
@@ -19,7 +19,6 @@ use Buckaroo\Woocommerce\Gateways\In3\In3Gateway;
 use Buckaroo\Woocommerce\Gateways\Kbc\KbcGateway;
 use Buckaroo\Woocommerce\Gateways\Klarna\KlarnaKpGateway;
 use Buckaroo\Woocommerce\Gateways\Klarna\KlarnaPayGateway;
-use Buckaroo\Woocommerce\Gateways\KnakenSettle\KnakenSettleGateway;
 use Buckaroo\Woocommerce\Gateways\Bizum\BizumGateway;
 use Buckaroo\Woocommerce\Gateways\MbWay\MbWayGateway;
 use Buckaroo\Woocommerce\Gateways\Multibanco\MultibancoGateway;
@@ -60,7 +59,6 @@ class PaymentGatewayRegistry
         'kbc' => ['gateway_class' => KbcGateway::class],
         'klarnakp' => ['gateway_class' => KlarnaKpGateway::class],
         'klarnapay' => ['gateway_class' => KlarnaPayGateway::class],
-        'knaken' => ['gateway_class' => KnakenSettleGateway::class],
         'mbway' => ['gateway_class' => MbWayGateway::class],
         'multibanco' => ['gateway_class' => MultibancoGateway::class],
         'przelewy24' => ['gateway_class' => Przelewy24Gateway::class],
